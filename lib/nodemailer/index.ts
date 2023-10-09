34f6539c28e78a7681fec4493b3380aa608991eb
hamzasdiri <sdirihamza241@gmail.com>
2023-10-09 19:29:41 +0200

    Create nodemailer function for sending specefic mails

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -28,7 +28,7 @@ export async function generateEmailBody(
       subject = `Welcome to Price Tracking for ${shortenedTitle}`;
       body = `
           <div>
-            <h2>Welcome to PriceWise 🚀</h2>
+            <h2>Welcome to ScrapeMaster 🚀</h2>
             <p>You are now tracking ${product.title}.</p>
             <p>Here's an example of how you'll receive updates:</p>
             <div style="border: 1px solid #ccc; padding: 10px; background-color: #f8f8f8;">
@@ -84,10 +84,13 @@ const transporter = nodemailer.createTransport({
   service: "hotmail",
   port: 2525,
   auth: {
-    user: "sdirihamza241@gmail.com",
+    user: "scrapemaster1@outlook.com",
     pass: process.env.EMAIL_PASSWORD,
   },
   maxConnections: 1,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const sendEmail = async (
@@ -95,14 +98,14 @@ export const sendEmail = async (
   sendTo: string[]
 ) => {
   const mailOptions = {
-    from: "sdirihamza241@gmail.com",
+    from: "scrapemaster1@outlook.com",
     to: sendTo,
     html: emailContent.body,
     subject: emailContent.subject,
   };
 
   transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if (error) return console.log(error);
+    if (error) return console.log("error while sending mail", error);
 
     console.log("Email sent: ", info);
   });
